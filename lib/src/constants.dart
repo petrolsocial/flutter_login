c181187f91fe80d7f37c233e8bfa33f75d55f97a
petrol-mallik <mallik@petrol.social>
2022-01-15 00:05:24 +0530

    Increasing max logo size

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -6,4 +6,4 @@ const kRecoverPasswordDescriptionKey = Key('RECOVER_PASSWORD_DESCRIPTION');
 const kDebugToolbarKey = Key('DEBUG_TOOLBAR');
 
 const kMinLogoHeight = 50.0; // hide logo if less than this
-const kMaxLogoHeight = 125.0;
+const kMaxLogoHeight = 200.0;
